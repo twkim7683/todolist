af835c7fc10494ac69b457ba0a2f8754881cc507
twkim <twkim7683@gmail.com>
2023-10-23 20:20:46 +0900

    first commit

---
+++ src/Goal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Goal extends Component {
+    render() {
+      return (
+        <div>
+          <ul>
+            <li>title</li>
+            <li>desc</li>
+            <li>importance</li>
+            <li>deadline</li>
+          </ul>
+        </div>
+      );
+  }
+}
+export default Goal;
